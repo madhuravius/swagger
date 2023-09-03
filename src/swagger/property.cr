50750cee2646428e6f390f62b774aef8a5faec4d
Madhusudan Ravi <madhuravius@protonmail.com>
2023-09-03 10:34:11 -0400

    chore: made code work

--- src/swagger/property.cr
+++ src/swagger/property.cr
@@ -1,5 +1,13 @@
+require "json"
+
 module Swagger
   struct Property
+    include JSON::Serializable
+
+    def self.use_reference(name : String)
+      new(ref: "#/components/schemas/#{name}")
+    end
+
     property name
     property type
     property format
@@ -8,10 +16,12 @@ module Swagger
     property default_value
     property example
     property required
-    property ref
+
+    @[JSON::Field(key: "$ref")]
+    getter ref : String? = nil
 
     def initialize(@name : String, @type : String = "string", @format : String? = nil,
-                   @items : (Object | String)? = nil, @description : String? = nil,
+                   @items : String? = nil, @description : String? = nil,
                    @default_value : (String | Int32 | Int64 | Float64 | Bool)? = nil,
                    @example : (String | Int32 | Int64 | Float64 | Bool)? = nil,
                    @required : Bool? = nil, @ref : String? = nil)
